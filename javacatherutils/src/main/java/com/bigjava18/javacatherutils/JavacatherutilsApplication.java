84f8a2f2875ccf3c1cfe2116bdcbd2cf9840b73e
zgp <2088220905@qq.com>
2021-09-06 11:20:02 +0800

    javacatherutils

---
+++ javacatherutils/src/main/java/com/bigjava18/javacatherutils/JavacatherutilsApplication.java
@@ -0,0 +1,13 @@
+package com.bigjava18.javacatherutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacatherutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacatherutilsApplication.class, args);
+    }
+
+}
---
+++ javacatherutils/src/test/java/com/bigjava18/javacatherutils/JavacatherutilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigjava18.javacatherutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacatherutilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
